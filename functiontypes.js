fe1504adc88e1ee0ff29164676052c15201036f8
23kb1a30d1-hash <23kb1a30d1@nbkrist.org>
2025-09-22 14:11:58 +0530

    Add files via upload

---
+++ functiontypes.js
@@ -0,0 +1,16 @@
+
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+
+const add = function (a, b) {
+    return a + b;
+};
+
+
+const multiply = (x, y) => x * y;
+
+
+console.log(greet("Alice"));
+console.log(add(5, 3));
+console.log(multiply(4, 7));       
